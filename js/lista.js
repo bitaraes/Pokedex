b15241ecbd07b8ceaab06686adb4011cc23eb7a6
Bitarães <igor_oliveira02@hotmail.com>
2021-04-23 04:22:24 -0300

    Add navegação e mudança de página lista

--- js/lista.js
+++ js/lista.js
@@ -1,23 +1,12 @@
-mudaPagina()
-criarLista(1)
+var paginaAtual = 1
+criarLista(paginaAtual)
 
 function buscarListagem(urlPrincipal, listagem) {
 	return fetch(urlPrincipal);
 };
-//Insere os itens html na lista
-function criarHtml(pokemonList, paginaAtual){
-	var listItens = document.querySelectorAll("div[class=list-item]");
-	listItens = Array.from(listItens);
-	for (i in listItens){
-		var dados = `<div class="list-nome caracteristica"><h2>${pokemonList[i].Nome}</h2></div>
-					<div class="list-img"><img src="${pokemonList[i].Img}"</div>
-					<div class="list-numero caracteristica">Número: ${pokemonList[i].Numero}</div>
-					<div class="list-tipo caracteristica">Tipos: ${pokemonList[i].Tipo} ${pokemonList[i].Tipo2}</div>`
-		listItens[i].innerHTML = dados;
-	}
-}
+
 //Faz a requisição da lista e cria um objeto com os dados dos pokemons
-async function criarLista(paginaAtual){
+async function criarLista(paginaAtual){	
 	var urlPagina = Number((paginaAtual-1)*20)
 	var urlPrincipal = `https://pokeapi.co/api/v2/pokemon?offset=${urlPagina}&limit=20`;
 	try{
@@ -50,18 +39,73 @@ async function criarLista(paginaAtual){
 	criarHtml(pokemonList, paginaAtual);
 }
 
+//Insere os itens html na lista
+function criarHtml(pokemonList, paginaAtual){
+	var listItens = document.querySelectorAll("div[class=list-item]");
+	listItens = Array.from(listItens);
+	for (i in listItens){
+		var dados = `<div class="list-nome caracteristica"><h2>${pokemonList[i].Nome}</h2></div>
+					<div class="list-img"><img src="${pokemonList[i].Img}"</div>
+					<div class="list-numero caracteristica">Número: ${pokemonList[i].Numero}</div>
+					<div class="list-tipo caracteristica">Tipos: ${pokemonList[i].Tipo} ${pokemonList[i].Tipo2}</div>`
+		listItens[i].innerHTML = dados;
+	}
+	if (paginaAtual > 2) {
+		if (paginaAtual > 53) {
+			document.querySelector("#paginacao").innerHTML = `
+			<span class="pag-num"><a href="">52</a></span>
+			<span class="pag-num"><a href="">53</a></span>
+			<span class="pag-num"><a href="">54</a></span>
+			<span class="pag-num"><a href="">55</a></span>
+			<span class="pag-num"><a href="">56</a></span>`
+		} else {
+			var paginas = { Penultima:paginaAtual-2,
+							Ultima:paginaAtual-1,
+							Atual:paginaAtual,
+							Sucessora: paginaAtual+1,
+							Proxima: paginaAtual+2
+						}
+			document.querySelector("#paginacao").innerHTML = `
+			<span class="pag-num"><a href="">1</a></span>
+			<span class="dot"></span><span class="dot"></span><span class="dot"></span>
+			<span class="pag-num"><a href="">${paginas.Penultima}</a></span>
+			<span class="pag-num"><a href="">${paginas.Ultima}</a></span>
+			<span class="pag-num"><a href="">${paginas.Atual}</a></span>
+			<span class="pag-num"><a href="">${paginas.Sucessora}</a></span>
+			<span class="pag-num"><a href="">${paginas.Proxima}</a></span>
+			<span class="dot"></span><span class="dot"></span><span class="dot"></span>
+			<span class="pag-num"><a href="">56</a></span>
+			`
+		}
+
+	} else {
+		document.querySelector("#paginacao").innerHTML = `
+		<span class="pag-num"><a href="">1</a></span>
+		<span class="pag-num"><a href="">2</a></span>
+		<span class="pag-num"><a href="">3</a></span>
+		<span class="pag-num"><a href="">4</a></span>
+		<span class="pag-num"><a href="">5</a></span>
+		<span class="dot"></span><span class="dot"></span><span class="dot"></span>
+		<span class="pag-num"><a href="">56</a></span>
+		`
+	}
+	mudaPagina()
+}
+
 //Faz a transição entre os itens da lista
 function mudaPagina(){
+	//Selecionar os números das páginas
 	numeroPagina = document.querySelectorAll("#paginacao a");
 	numeroPagina = Array.from(numeroPagina)
+	//Caputurar o clique de mudança de página
 	for (i=0; i < numeroPagina.length; i++){
+		console.log(numeroPagina[i])
 		numeroPagina[i].addEventListener("click", function(evento){
 			evento.preventDefault();
 			var paginaAtual = parseInt(this.text);
 			criarLista(paginaAtual);
 		})
 	}
-
 }
 
 
